74272aa2a0a5f0c42ef2d1a31404458fb19f007c
Pedro Ferreira - Ferrodude <pedroferrodude@hotmail.com>
2024-12-31 14:40:10 +0000

    removed test elements, added configs for prettier and ts

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,8 @@
-import { Button } from "@/components/ui/button"
-
 function App() {
-  const testeFunc = () => {
-    console.log('Teste');
-  };
-
   return (
     <>
-      <h1>Teste APP component</h1>
-     
-      <Button variant="default" size="default" onClick={testeFunc}>Teste Button</Button>
+      <h1>Win My Argument</h1>
     </>
-
   );
 }
 
